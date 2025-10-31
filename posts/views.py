8aee74ebbfd2a39f3565f24ea9d460c8173de1a6
AkosuaSobolo <akosuaworde11@gmail.com>
2025-10-31 21:11:09 +0000

    Initial commit  my django practice project

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from . models import post
+from django.contrib.auth.decorators import login_required
+from . import forms
+
+# Create your views here.
+def posts_list(request):
+    posts = post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    Post = post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': Post})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form': form})
